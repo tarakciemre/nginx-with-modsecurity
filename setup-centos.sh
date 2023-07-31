cdacd44b67f0ab1a89d4987c27f0b0e105fa9823
Emre Tarakci <tarakci.software@gmail.com>
2023-07-31 14:13:44 +0300

    Update included config files' order

--- setup-centos.sh
+++ setup-centos.sh
@@ -1,7 +1,7 @@
 # copy certificate into /etc/pki/ca-trust/source/anchors/
 # copy the rest of the commands to the root as well
 docker build --tag centos-modsec .
-docker stop modsecurity
+docker kill modsecurity
 docker rm modsecurity 2> /dev/null
 gnome-terminal -- sh -c "docker run --privileged=true -v /sys/fs/cgroup:/sys/fs/cgroup:ro -p 8080:443 --name modsecurity -it centos-modsec /usr/sbin/init"
 sleep 5
@@ -19,3 +19,4 @@ echo "running script script..."
 #docker exec -it modsecurity bash -c "/root/in-centos-setup.sh"
 
 gnome-terminal -- sh -c "docker exec -it modsecurity /bin/bash"
+~/scripts/keyboard_layout.sh
